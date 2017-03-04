5fdb5a2e4d11d0ceca19706e48a5f2b82fb2a6af
aym35 <aydin@aym.co>
2017-03-04 12:39:27 -0500

    Added a displayControls object and some code cleanup

--- minesweeper-aydin.js
+++ minesweeper-aydin.js
@@ -1,8 +1,38 @@
+/**********
+ToDos: 
+1) make sure colors are defined at top
+1.5) when instantiating displayControls, are we forgetting to pass in numberOfMines and does it matter
+2) still a bug if one full column or row of " " cell innerHTML values
+3) maybe change cells into buttons
+**********/
+
 var table = document.createElement("table");
 table.id = "myTable";
 
-var mineDisplay = document.getElementById("mines-left");
-var timeDisplay = document.getElementById("time-elapsed");
+function displayControls(numberOfMines) {
+	var mineDisplayValue = numberOfMines;
+	var timeDisplayValue = 0;
+	var mineDisplay = document.getElementById("mines-left");
+	var timeDisplay = document.getElementById("time-elapsed");
+
+	this.returnMineDisplay = function() {
+		return mineDisplayValue;
+	}
+	this.setMineDisplay = function(number) {
+		mineDisplay.innerHTML = mineDisplayValue = number;
+	}
+	this.returnTimeDisplay = function() {
+		return timeDisplayValue;
+	}
+	this.setTimeDisplay = function(number) {
+		timeDisplay.innerHTML = timeDisplayValue = number;
+	}
+}
+displayControls.prototype.adjustMineDisplay= function(number) {
+	this.setMineDisplay((this.returnMineDisplay() + number));
+}
+
+
 
 function board() {
 	this.rows = 8;
@@ -102,10 +132,11 @@ board.prototype.showCells = function () {
 function game() {
 	this.timeElapsed = 0;
 	this.myBoard = new board();
+	this.myControls = new displayControls();
 	var that = this;	
 	this.incrementTimer = function() {
 		that.timeElapsed += 1;
-		timeDisplay.innerHTML = that.timeElapsed;
+		that.myControls.setTimeDisplay(that.timeElapsed);
 	}
 }
 game.prototype.initialize = function () {
@@ -115,7 +146,7 @@ game.prototype.initialize = function () {
 	this.myBoard.countAdjacentMines();
 
 	document.getElementById('board').appendChild(table);
-	mineDisplay.innerHTML = this.myBoard.mines;
+	this.myControls.setMineDisplay(this.myBoard.mines);
 }
 game.prototype.incrementTimer = function () {
 	this.timeElapsed += 1;
@@ -141,7 +172,7 @@ game.prototype.checkIfWon = function (varBoard, varMine) {
 }
 game.prototype.youWin =  function () { //stop timer and record winner name and time in table
 	var winningTime = this.timeElapsed;
-	clearInterval(timer);
+	clearInterval(this.timer);
 	alert('your winning time is: ' + winningTime);
 	var name = prompt("What is your name?", "N/A");
 	var tdName = document.createElement("td");
@@ -162,11 +193,10 @@ game.prototype.restart = function () {
 	this.myBoard.generateMines();
 	this.myBoard.plantMines();
 	this.myBoard.countAdjacentMines();
-	mineDisplay.innerHTML = this.myBoard.mines;
+	this.myControls.setMineDisplay(this.myBoard.mines);
 	this.startTimer();
 }
 game.prototype.addClickEvents = function () {
-
 	var that = this;
 	//Specify what happens on both click and right click
 	for (var i=0; i < this.myBoard.rows; i++) {
@@ -179,14 +209,14 @@ game.prototype.addClickEvents = function () {
 				if (this.innerHTML == "?") updateMineValue(1); //If a "?", this means it's a suspected mine. Since you'll be removing the suspected mine by clicking on it, we should increment the minesLeft count
 				var cellValue = that.myBoard.hiddenBoard[(this.id[0])][(this.id[2])];
 				if( cellValue == "*") alert('game over');
-				else if ( cellValue == "-") recursiveLoop(this.id, that.myBoard);
+				else if ( cellValue == "-") that.recursiveLoop(this.id, that.myBoard);
 				else {
 					this.innerHTML = cellValue; 
 					this.style.background = "#e0e0e0";
-					this.style.color = determineColor(cellValue);
+					this.style.color = that.determineColor(cellValue);
 				} 
 				//Check if you won the game
-				that.checkIfWon(that.myBoard, parseInt(mineDisplay.innerHTML));
+				that.checkIfWon(that.myBoard, that.myControls.returnMineDisplay());
 			});
 			
 			//On Right Click
@@ -196,37 +226,19 @@ game.prototype.addClickEvents = function () {
 				if (this.innerHTML != "?") {
 					this.innerHTML = "?";
 					this.style.background = '#ff2b47';
-					updateMineValue(-1);
+					that.myControls.adjustMineDisplay(-1);
 				} else {
 					this.innerHTML = "&nbsp;";
 					this.style.removeProperty("background");
-					updateMineValue(1);
+					that.myControls.adjustMineDisplay(1);
 				}
 				//Now check if you won the game
-				that.checkIfWon(that.myBoard, parseInt(mineDisplay.innerHTML));
+				that.checkIfWon(that.myBoard, that.myControls.returnMineDisplay());
 			});
 		}
 	}
 }
-
-
-
-var myGame = new game();
-myGame.initialize();
-myGame.addClickEvents(); //add the left-click and right-click event listeners
-
-
-
-function returnMineValue() {
-	return (parseInt(mineDisplay.innerHTML));
-}
-function updateMineValue(number) {
-	var currentMineValue = returnMineValue();
-	currentMineValue = currentMineValue + number;
-	mineDisplay.innerHTML = currentMineValue;
-}
-
-function recursiveLoop(id, myBoard1) {
+game.prototype.recursiveLoop = function(id, myBoard1) {
 	var i = parseInt(id[0]);
 	var j = parseInt(id[2]);
 	var initialCell = document.getElementById(id);
@@ -245,16 +257,15 @@ function recursiveLoop(id, myBoard1) {
 				if ( realValue != "*" && cell.innerHTML == "&nbsp;") {
 					cell.innerHTML = realValue;
 					cell.style.background = "#e0e0e0";
-					cell.style.color = determineColor(realValue);
-					if (realValue == "-") recursiveLoop(val1 + "-" + val2,myBoard1);
+					cell.style.color = this.determineColor(realValue);
+					if (realValue == "-") this.recursiveLoop(val1 + "-" + val2,myBoard1);
 				} 
 			}	
 			catch (err) {}
 		}
 	}	
 }
-
-function determineColor (realValue) {
+game.prototype.determineColor = function(realValue) {
 	switch (realValue) {
 		case 1:
 			return "blue";
@@ -270,3 +281,15 @@ function determineColor (realValue) {
 			return "black";
 	}
 }
+
+
+
+var myGame = new game();
+myGame.initialize();
+myGame.addClickEvents(); //add the left-click and right-click event listeners
+
+
+
+
+
+
